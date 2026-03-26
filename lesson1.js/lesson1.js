708b1ebad8f96d66a7696df246bc1ad22421b041
leon.lone <leonmuriithi.kobia@gmail.com>
2026-03-26 10:34:34 +0300

    msg

---
+++ lesson1.js/lesson1.js
@@ -0,0 +1,14 @@
+// your code goes here
+
+
+name = "Leon Kobia"
+age = 19
+weight = 82
+fav_dish = "Zote"
+f_number = 45
+s_number = 15
+
+console.log("Hello from " +  name)
+console.log("I am " + age +" years old")
+sum_numbers = f_number + s_number
+console.log("Sum of the number is " + sum_numbers)
---
+++ lesson1.js/lesson2.js
@@ -0,0 +1,15 @@
+//This is a comment
+/*This
+is
+a multiline
+comment*/
+
+
+age = 12
+
+if(age >= 18){
+    console.log("You are allowed to drive")
+    
+}else{
+    console.log("You are not allowed to drive")
+}
